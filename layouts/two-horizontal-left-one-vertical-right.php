83cebee6c63ebdd65695568f79fff36010781b6c
Vontainment <djav1985@gmail.com>
2025-08-04 23:37:39 -0400

    Add two-horizontal-left-one-vertical-right layout

---
+++ layouts/two-horizontal-left-one-vertical-right.php
@@ -0,0 +1,5 @@
+<div class="layout two-horizontal-left-one-vertical-right">
+    <div class="panel panel1" data-slot="1"></div>
+    <div class="panel panel2" data-slot="2"></div>
+    <div class="panel panel3" data-slot="3"></div>
+</div>
