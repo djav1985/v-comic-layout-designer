efb9f26a1f9c05e8fcddce339c4c0ce810f65a92
Vontainment <djav1985@gmail.com>
2025-08-04 23:37:36 -0400

    Add layout with top horizontal and three vertical panels

---
+++ layouts/one-horizontal-top-three-vertical-bottom.php
@@ -0,0 +1,6 @@
+<div class="layout one-horizontal-top-three-vertical-bottom">
+    <div class="panel panel1" data-slot="1"></div>
+    <div class="panel panel2" data-slot="2"></div>
+    <div class="panel panel3" data-slot="3"></div>
+    <div class="panel panel4" data-slot="4"></div>
+</div>
