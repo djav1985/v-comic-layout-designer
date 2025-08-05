91676b28591fa5519d3034e2ab333571a4bd244d
Vontainment <djav1985@gmail.com>
2025-08-04 23:38:05 -0400

    Add four grid layout

---
+++ layouts/four-grid.php
@@ -0,0 +1,6 @@
+<div class="layout four-grid">
+    <div class="panel panel1" data-slot="1"></div>
+    <div class="panel panel2" data-slot="2"></div>
+    <div class="panel panel3" data-slot="3"></div>
+    <div class="panel panel4" data-slot="4"></div>
+</div>
